ecdfb59fd5fb8bcb528b03add6fa2456c07b1338
ararrrr <rr.sena38@gmail.com>
2026-05-29 00:37:24 +0800

    Fix frontend API URL

--- sena-client/src/services/UserService.js
+++ sena-client/src/services/UserService.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
-import constants from '../constants';
 
-// API Access to Front-end JSON data transformation or decoder
 const API = axios.create({
-  baseURL: `${constants.HOST}/users`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/users`,
 });
 
 // Fetch users
-export const fetchUsers = (user) => API.get('/', user);
+export const fetchUsers = () => API.get('/');
 
 // Create user
 export const createUser = (user) => API.post('/', user);
